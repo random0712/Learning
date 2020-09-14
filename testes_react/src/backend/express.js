f8d197916b724e29894815e013969b012225132e
random0712 <patricknuvem9@gmail.com>
2020-09-14 18:06:34 -0300

    Integrando plugin do MySql no projeto

--- testes_react/src/backend/express.js
+++ testes_react/src/backend/express.js
@@ -1,6 +1,12 @@
 const app = require('express')()
+const connection =  require('./config/mysql')
 const port = 3001
 
+connection.ping(function(err) {
+    if(err) throw err
+
+    console.log('Funcionando')
+})
 
 app.listen(port, () => {
     console.log(`Backend rodando na porta ${port}`)
